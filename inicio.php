d1df3c861eb937274fd074ed07d228f09850998b
Mauricio <mauriciobermudez@hotmail.com>
2019-10-12 20:27:51 -0600

    fixing merge conflicts

--- inicio.php
+++ inicio.php
@@ -1,1 +1,17 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Progressive Web Application">
+    <meta name="theme-color" content="#499bea" />
+    <title>Inicio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="manifest.json">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/css_inicio.css" />
+    
+</head>
+<body>
+</body>
+</html>
